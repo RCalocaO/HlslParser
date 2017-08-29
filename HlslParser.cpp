1b2f511f2be7a6ce3117321e39af9a6394393a0e
Rolando Caloca <rcaloca@gmail.com>
2017-08-28 23:22:16 -0400

    Floats

--- HlslParser.cpp
+++ HlslParser.cpp
@@ -30,6 +30,7 @@ enum class EToken
 {
 	Error,
 	Integer,
+	Float,
 	Plus,
 	Multiply,
 	Minus,
@@ -61,8 +62,22 @@ struct FLexer
 		End = Data + Source.length();
 	}
 
+	void SkipWhiteSpace()
+	{
+		char c = *Data;
+		assert(c);
+		FToken Token;
+
+		while (Data < End && (c == ' ' || c == '\n' || c == '\t' || c == '\r'))
+		{
+			++Data;
+			c = *Data;
+		}
+	}
+
 	bool HasMoreTokens()
 	{
+		SkipWhiteSpace();
 		if (Data >= End)
 		{
 			assert(Data == End);
@@ -73,16 +88,11 @@ struct FLexer
 
 	FToken NextToken()
 	{
+		FToken Token;
+		SkipWhiteSpace();
+
 		char c = *Data;
 		assert(c);
-		FToken Token;
-
-		while (c == ' ' || c == '\n' || c == '\t' || c == '\r')
-		{
-			++Data;
-			c = *Data;
-		}
-
 		c = *Data;
 		if (isdigit(c))
 		{
@@ -93,6 +103,31 @@ struct FLexer
 			} while (isdigit(*Data));
 
 			Token.TokenType = EToken::Integer;
+
+			if (*Data == '.')
+			{
+				Token.Literal += *Data++;
+				Token.TokenType = EToken::Float;
+				while (isdigit(*Data))
+				{
+					Token.Literal += *Data;
+					++Data;
+				}
+			}
+
+			if (*Data == 'e' || *Data == 'E')
+			{
+				Token.Literal += *Data++;
+				if (*Data == '-' || *Data =='+')
+				{
+					Token.Literal += *Data++;
+				}
+				while (isdigit(*Data))
+				{
+					Token.Literal += *Data;
+					++Data;
+				}
+			}
 		}
 		else
 		{
